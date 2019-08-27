331e973c7f407c48b6f79a492e7f8b540a46cba8
jabirsubaktagin16 <subaktagin.jabir16@outlook.com>
2019-08-27 11:04:20 +0600

    Contact Texttype

--- contact.php
+++ contact.php
@@ -102,19 +102,19 @@
               
               <div class="p-4 mb-3 bg-white">
                 <p class="mb-0 font-weight-bold">Address</p>
-                <p class="mb-4">203 Fake St. Mountain View, San Francisco, California, USA</p>
+                <p class="mb-4">56 Sabujbag, Dhaka, Bangladesh</p>
 
                 <p class="mb-0 font-weight-bold">Phone</p>
-                <p class="mb-4"><a href="#">+1 232 3235 324</a></p>
+                <p class="mb-4"><a href="#">+8801996846517</a></p>
 
                 <p class="mb-0 font-weight-bold">Email Address</p>
-                <p class="mb-0"><a href="#">youremail@domain.com</a></p>
+                <p class="mb-0"><a href="#">arif.ishan05@gmail.com</a></p>
               </div>
               
               <div class="p-4 mb-3 bg-white">
                 <img src="images/hero_bg_1.jpg" alt="Image" class="img-fluid mb-4 rounded">
                 <h3 class="h5 text-black mb-3">More Info</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ad iure porro mollitia architecto hic consequuntur. Distinctio nisi perferendis dolore, ipsa consectetur? Fugiat quaerat eos qui, libero neque sed nulla.</p>
+                <p>For More Info You can contact us <br> <a href="mailto:subaktagin.jabir16@gmail.com">Ahmad Subaktagin Jabir</a> <br> <a href="mailto:farzanamayisha11@gmail.com">Mayisha Farzana</a></p>
                 <p><a href="#" class="btn btn-primary px-4 py-2 text-white">Learn More</a></p>
               </div>
 
